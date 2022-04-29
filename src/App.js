1407de4cfcbf0a4bec0f8a9da2e0f17770c24387
berkeklc <berkeklc.123@gmail.com>
2022-04-29 10:14:59 +0300

    dropdown fix

--- src/App.js
+++ src/App.js
@@ -180,7 +180,7 @@ function App() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="origin-top-right absolute left-0 mt-2 w-48  shadow-lg py-1 bg-fuchsia-500 bg-opacity-20 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="origin-top-right relative left-0 mt-2 w-48  shadow-lg py-1 bg-fuchsia-500 bg-opacity-20 ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
                           <a
