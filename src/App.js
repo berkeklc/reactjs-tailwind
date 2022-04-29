037553f91f39b3f302c9030f3e1c95d06abce42a
berkeklc <berkeklc.123@gmail.com>
2022-04-29 11:19:18 +0300

    glow effect added

--- src/App.js
+++ src/App.js
@@ -281,11 +281,11 @@ function App() {
 
     <MediaComponent />
 
-  <p className="absolute left-24 bottom-36 font-custom1 opacity-0	hover:opacity-100 text-[#da5fc5]">CULTURE</p>
-  <p className="absolute right-24 bottom-48 font-custom1 opacity-0	hover:opacity-100 text-[#da5fc5]">CORE</p>
-  <p className="absolute left-64 top-36 font-custom1 opacity-0	hover:opacity-100 text-[#da5fc5]">CREATE</p>
-  <p className="absolute left-96 bottom-28 font-custom1 opacity-0	hover:opacity-100 text-[#da5fc5]">CHARM</p>
-  <p className="absolute right-24 top-68 font-custom1 opacity-0	hover:opacity-100 text-[#da5fc5]">CATALYST</p>
+  <p className="absolute left-24 bottom-36 glow font-custom1 opacity-0 p-5 text-2xl		 ease-in duration-300 hover:opacity-100 hover:ease-in hover:duration-500 text-[#da5fc5]">CULTURE</p>
+  <p className="absolute right-24 bottom-48 glow font-custom1 opacity-0 p-5 text-2xl		 ease-in duration-300 hover:opacity-100 hover:ease-in hover:duration-500 text-[#da5fc5]">CORE</p>
+  <p className="absolute left-64 top-36 glow font-custom1 opacity-0 p-5 text-2xl		 ease-in duration-300 hover:opacity-100 hover:ease-in hover:duration-500 text-[#da5fc5]">CREATE</p>
+  <p className="absolute left-96 bottom-28 glow font-custom1 opacity-0 p-5 text-2xl		 ease-in duration-300 hover:opacity-100 hover:ease-in hover:duration-500 text-[#da5fc5]">CHARM</p>
+  <p className="absolute right-24 top-72 glow font-custom1 opacity-0 p-5 text-2xl		 ease-in duration-300 hover:opacity-100 hover:ease-in hover:duration-500 text-[#da5fc5]">CATALYST</p>
     </>
   )
 }
