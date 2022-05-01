8003305552a7b49b1965c91a10a7e32558c74b3e
berkeklc <berkeklc.123@gmail.com>
2022-05-01 14:50:34 +0300

    css fix

--- src/components/Sound.js
+++ src/components/Sound.js
@@ -5,25 +5,23 @@ import song from "./../assets/sound.mp3";
 
 class Sound extends Component {
   state = {
-
     audio: new Audio(song),
-
+    play: true,
+    volume: 0.2,
     isPlaying: true,
     autoPlay: true,
+    loop: true,
   };
   
   playPause = () => {
 
     let isPlaying = this.state.isPlaying;
-    this.state.audio.load();
-
+    this.state.loop = true;
     if (isPlaying) {
       this.state.audio.play();
     } else {
-
       this.state.audio.pause();
     }
-    
     this.setState({ isPlaying: !isPlaying });
   };
 
@@ -32,7 +30,7 @@ class Sound extends Component {
       <div className="absolute z-[9999] bottom-10 left-10">
 
         <button  onClick={this.playPause}>
-        <Twitter className="h-8 fill-[#548bee]	hover:fill-white" />
+        <p className="colors font-custom1 text-white hover:text-blue-600">press x</p>
         </button>
       </div>
     );
